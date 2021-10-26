6e8c236a58d99297e16f6d596206861144479a9a
SifySM <90615357+SifySM@users.noreply.github.com>
2021-10-26 16:48:50 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package edu.vsu.ru;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int num = CNumber.readNumber();
+            CTestMonotony.findMonotoneNumber(num);
+        }
+    }
